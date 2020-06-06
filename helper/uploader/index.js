fe689127218e53fe7b0e66e6ff120b5be5f9d577
Ossama Rafique <contact@ossamarafique.com>
2020-06-06 18:37:11 +0500

    S3 Upload Fixed

--- helper/uploader/index.js
+++ helper/uploader/index.js
@@ -1,4 +1,4 @@
-const { S3 } = require('aws-sdk');
+const { S3, Endpoint } = require('aws-sdk');
 const mime = require('mime');
 const crypto = require('crypto');
 const path = require('path')
@@ -13,31 +13,34 @@ const isImage = filename => {
     return true;
 }
 
-const S3_Client = new S3({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET,
-    params: { Bucket: process.env.S3_BUCKET },
-});
-
 const uploadToS3 = async file =>{
     if(process.env.S3_ENABLED==="yes"){
+        const spacesEndpoint = new Endpoint(process.env.S3_REGION);
+        const conf = {
+            endpoint: spacesEndpoint,
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            params: { Bucket: process.env.S3_BUCKET },
+        };
+        const S3_Client = new S3(conf);
         try{
             const { createReadStream, filename, mimetype, encoding } = await file;
             if(!isImage(filename)){
                 throw new UserInputError('Only .jpg .png .gif are allowed');
             }
+            const uniqueString = await crypto.randomBytes(16).toString('hex') + Date.now();
+            const stream = await createReadStream();
             const response = await S3_Client
             .upload({
-              Key: filename,
+              Key: uniqueString+'-'+filename,
               ACL: 'public-read',
-              Body: await createReadStream()
+              Body: stream
             })
             .promise()
-        
-          return {
-            name: filename,
-            url: response.Location
-          }
+            return {
+                name: filename,
+                url: response.Location
+            }
         } catch(error){
             throw error;
         }
@@ -52,7 +55,7 @@ const uploadToDisk = async (file) => {
         if(!isImage(filename)){
             throw new UserInputError('Only .jpg .png .gif are allowed');
         }
-        const uniqueString = crypto.randomBytes(16).toString('hex');
+        const uniqueString = await crypto.randomBytes(16).toString('hex');
         const url = path.join("uploads/", uniqueString + Date.now() + '.' + mime.getExtension(mimetype));
         await new Promise((res) =>
         createReadStream()
